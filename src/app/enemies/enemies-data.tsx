f7472349dfa2feb71305e9e0d4c531cb7dc44108
Pedro Costa <pedrocostavgs@gmail.com>
2025-05-22 09:39:15 -0300

    feat: init enemies page

---
+++ src/app/enemies/enemies-data.tsx
@@ -0,0 +1,43 @@
+export const Enemies = [
+    {
+        id: 1,
+        name: "Crawlid"
+    },
+    {
+        id: 2,
+        name: "Vengefly"
+    },
+    {
+        id: 3,
+        name: "Vengefly King"
+    },
+    {
+        id: 4,
+        name: "Gruzzer"
+    },
+    {
+        id: 5,
+        name: "Gruz Mother"
+    },
+    {
+        id: 6,
+        name: "Tiktik"
+    },
+    {
+        id: 7,
+        name: "Aspid Hunter"
+    },
+    {
+        id: 8,
+        name: "Aspid Mother"
+    },
+    {
+        id: 9,
+        name: "Aspid Hatchling"
+    },
+    {
+        id: 10,
+        name: "Goam"
+    }
+
+]
--- src/app/enemies/page.tsx
+++ src/app/enemies/page.tsx
@@ -1,17 +1,46 @@
+"use client";
+
+import { useState } from "react";
 import { Header } from "../header";
+import { Enemies } from "./enemies-data";
 
 export default function EnemiesPage() {
+  const [search, setSearch] = useState("");
+
+  const filteredEnemies = Enemies.filter((Enemies) =>
+    Enemies.name.toLowerCase().includes(search.toLowerCase())
+  );
+  console.log(search, filteredEnemies, Enemies);
+
   return (
-    <div className="h-screen w-full bg-black">
+    <div className="min-h-screen w-full bg-black">
       <Header />
-      <main className="w-[90%] m-auto xl:w-full text-center py-10 px-4">
+      <main className="w-[90%] m-auto xl:w-full text-center py-10 px-4 h-fit">
         <h1 className="text-[#FFF] text-[32px] font-bold font-cinzel">
           Enemies
         </h1>
-        <p className="max-w-[1000px] m-auto">
+        <p className="max-w-[1400px] m-auto mb-8">
           Track your progress through Hallownest with these challenging feats of
           skill and exploration.
         </p>
+        <input
+          type="text"
+          placeholder="Grimm..."
+          className="border border-[#505050] rounded-[8px] px-4 py-2 w-full max-w-[1400px] focus:outline-0"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+
+        <ul className="grid grid-cols-5 gap-16 p-16 overflow-hidden">
+          {filteredEnemies.map((enemy) => (
+            <li
+              key={enemy.id}
+              className="border border-[#505050] p-8 rounded-[8px] h-[400px]"
+            >
+              <h2 className="text-[#FFF]">{enemy.name}</h2>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
